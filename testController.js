0060823a226bf6dbae52300efef5b63611fbc7b2
Евгени Жечев <evgeni.jechev@upnetix.com>
2018-03-27 16:02:58 +0300

    3 day forecast added

--- testController.js
+++ testController.js
@@ -1,8 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('home').addEventListener('click', function() {
-        console.log('vleze')
-        getWeather('sofia').then(function(data) {
-            console.log(data)
+        function getCity() {
+            return document.getElementById('city').value
+        }
+    document.getElementById('enterCity').addEventListener('click', function() {
+        let city = document.getElementById('city').value 
+        getWeather(getCity()).then(function(data) {
+            let weatherToday = data;
+            document.getElementById('infoWraper').style.display = 'block';
+            document.getElementById('cityName').textContent = `Град: ${weatherToday.name}`;
+            document.getElementById('weatherDescription').textContent = `Днес времето ще бъде ${weatherToday.weather[0].description}`;
+            document.getElementById('weatherTemps').textContent = `Температурата в момента е ${Math.round(weatherToday.main.temp - 273.15)} градуса`
+        })
+        
+    })
+    document.getElementById('seeForecast').addEventListener('click',function() {
+        
+        getForecast(getCity()).then(function(data) {
+            document.getElementById('forecast').style.display = 'block';
+            document.getElementById('imgDayOne').src = `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`
+            document.getElementById('dayOne').textContent = `Утре времето ще бъде ${data.list[0].weather[0].description}`;
+            document.getElementById('dayOneTemp').textContent = `Със средна температура от ${Math.round(data.list[0].main.temp - 273.15)} градуса`;
+            
+            document.getElementById('imgDayTwo').src = `http://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png`
+            document.getElementById('dayTwo').textContent = `В други ден времето ще бъде ${data.list[1].weather[0].description}`;
+            document.getElementById('dayTwoTemp').textContent = `Със средна температура от ${Math.round(data.list[1].main.temp - 273.15)} градуса`;
+            
+            document.getElementById('imgDayThree').src = `http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png`
+            document.getElementById('dayThree').textContent = `След 3 дена времето ще бъде ${data.list[2].weather[0].description}`;
+            document.getElementById('dayThreeTemp').textContent = `Със средна температура от ${Math.round(data.list[2].main.temp - 273.15)} градуса`;
+            
+            
         })
     })
+
+
 })
--- testModel.js
+++ testModel.js
@@ -1,14 +1,29 @@
 function getWeather(city) {
     return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest()
+        let xhr = new XMLHttpRequest();
         xhr.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ae077bc411e863a6ab7ead0fcacdb38`, true);
-        xhr.send(null)
+        xhr.send(null);
 
         xhr.addEventListener('load', function() {
             if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(JSON.parse(xhr.responseText))
+                resolve(JSON.parse(xhr.responseText));
             } else {
-                reject(xhr.status)
+                reject(xhr.status);
+            }
+        })
+    })
+}
+
+function getForecast(city) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3ae077bc411e863a6ab7ead0fcacdb38`, true);
+        xhr.send(null);
+        xhr.addEventListener('load', function() {
+            if(xhr.status >= 200 && xhr.status < 300) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(xhr.status);
             }
         })
     })
