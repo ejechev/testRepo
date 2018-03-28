abb43b332dba6a5ec644843975b93a15494a1bdc
Евгени Жечев <evgeni.jechev@upnetix.com>
2018-03-28 11:02:12 +0300

    TEST_code review fixes

--- testController.js
+++ testController.js
@@ -1,37 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-        function getCity() {
-            return document.getElementById('city').value
-        }
-    document.getElementById('enterCity').addEventListener('click', function() {
-        let city = document.getElementById('city').value 
-        getWeather(getCity()).then(function(data) {
+document.addEventListener('DOMContentLoaded', function () {
+    function getCity() {
+        return document.getElementById('city').value
+    }
+
+    var getTodayWeather = function () {
+        document.getElementById('forecast').style.display = 'none'
+        getWeather(`${API_URL}/data/2.5/weather?q=${getCity()}&appid=${OPENWEATHER_KEY}`).then(function (data) {
             let weatherToday = data;
-            document.getElementById('infoWraper').style.display = 'block';
-            document.getElementById('cityName').textContent = `Град: ${weatherToday.name}`;
-            document.getElementById('weatherDescription').textContent = `Днес времето ще бъде ${weatherToday.weather[0].description}`;
-            document.getElementById('weatherTemps').textContent = `Температурата в момента е ${Math.round(weatherToday.main.temp - 273.15)} градуса`
+            document.getElementById('info-wrapper').style.display = 'block';
+            document.getElementById('city-name').textContent = `Град: ${weatherToday.name}`;
+            document.getElementById('weather-description').textContent = `Днес времето ще бъде ${weatherToday.weather[0].description}`;
+            document.getElementById('weather-temps').textContent = `Температурата в момента е ${Math.round(weatherToday.main.temp - KELVIN_CONSTANT)} градуса`
+        }).catch(function (error) {
+            throw new Error(error.message)
         })
-        
-    })
-    document.getElementById('seeForecast').addEventListener('click',function() {
-        
-        getForecast(getCity()).then(function(data) {
-            document.getElementById('forecast').style.display = 'block';
-            document.getElementById('imgDayOne').src = `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`
-            document.getElementById('dayOne').textContent = `Утре времето ще бъде ${data.list[0].weather[0].description}`;
-            document.getElementById('dayOneTemp').textContent = `Със средна температура от ${Math.round(data.list[0].main.temp - 273.15)} градуса`;
-            
-            document.getElementById('imgDayTwo').src = `http://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png`
-            document.getElementById('dayTwo').textContent = `В други ден времето ще бъде ${data.list[1].weather[0].description}`;
-            document.getElementById('dayTwoTemp').textContent = `Със средна температура от ${Math.round(data.list[1].main.temp - 273.15)} градуса`;
-            
-            document.getElementById('imgDayThree').src = `http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png`
-            document.getElementById('dayThree').textContent = `След 3 дена времето ще бъде ${data.list[2].weather[0].description}`;
-            document.getElementById('dayThreeTemp').textContent = `Със средна температура от ${Math.round(data.list[2].main.temp - 273.15)} градуса`;
-            
-            
+
+    }
+    document.getElementById('enter-city').addEventListener('click', getTodayWeather)
+
+
+    var getThreeDayForecasat = function () {
+        getForecast(`${API_URL}/data/2.5/forecast?q=${getCity()}&appid=${OPENWEATHER_KEY}`).then(function (data) {
+            if (data.list[0]) {
+                document.getElementById('forecast').style.display = 'block';
+                document.getElementById('img-day-one').src = `${API_URL}/img/w/${data.list[0].weather[0].icon}.png`
+                document.getElementById('day-one').textContent = `Утре времето ще бъде ${data.list[0].weather[0].description}`;
+                document.getElementById('day-one-temp').textContent = `Със средна температура от ${Math.round(data.list[0].main.temp - KELVIN_CONSTANT)} градуса`;
+            } else {
+                document.getElementById('day-one').textContent = `Липсва информация за този ден`
+            }
+
+            if (data.list[1]) {
+                document.getElementById('img-day-two').src = `${API_URL}/img/w/${data.list[1].weather[0].icon}.png`
+                document.getElementById('day-two').textContent = `В други ден времето ще бъде ${data.list[1].weather[0].description}`;
+                document.getElementById('day-two-temp').textContent = `Със средна температура от ${Math.round(data.list[1].main.temp - KELVIN_CONSTANT)} градуса`;
+            } else {
+                document.getElementById('day-one').textContent = `Липсва информация за този ден`
+            }
+
+            if (data.list[2]) {
+                document.getElementById('img-day-three').src = `${API_URL}/img/w/${data.list[2].weather[0].icon}.png`
+                document.getElementById('day-three').textContent = `След 3 дена времето ще бъде ${data.list[2].weather[0].description}`;
+                document.getElementById('day-three-temp').textContent = `Със средна температура от ${Math.round(data.list[2].main.temp - KELVIN_CONSTANT)} градуса`;
+            } else {
+                document.getElementById('day-one').textContent = `Липсва информация за този ден`
+            }
+
+        }).catch(function (error) {
+            throw new Error(error.message)
         })
-    })
+    }
+    document.getElementById('see-forecast').addEventListener('click', getThreeDayForecasat);
 
 
 })
--- testModel.js
+++ testModel.js
@@ -1,30 +1,42 @@
-function getWeather(city) {
+var OPENWEATHER_KEY = '3ae077bc411e863a6ab7ead0fcacdb38';
+var API_URL = 'http://api.openweathermap.org';
+var KELVIN_CONSTANT = 273.15;
+
+function getWeather(weatherURL) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ae077bc411e863a6ab7ead0fcacdb38`, true);
+        xhr.open('GET', weatherURL, true);
         xhr.send(null);
 
-        xhr.addEventListener('load', function() {
-            if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(JSON.parse(xhr.responseText));
+        xhr.addEventListener('load', function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                if (xhr.getResponseHeader('content-type').indexOf('json') > -1) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    throw new Error('Response is not valid JSON file')
+                };
             } else {
-                reject(xhr.status);
-            }
-        })
-    })
-}
+                reject(JSON.parse(xhr.responseText));
+            };
+        });
+    });
+};
 
-function getForecast(city) {
-    return new Promise(function(resolve, reject) {
+function getForecast(weatherURL) {
+    return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3ae077bc411e863a6ab7ead0fcacdb38`, true);
+        xhr.open('GET', weatherURL, true);
         xhr.send(null);
-        xhr.addEventListener('load', function() {
-            if(xhr.status >= 200 && xhr.status < 300) {
-                resolve(JSON.parse(xhr.responseText));
+        xhr.addEventListener('load', function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                if (xhr.getResponseHeader('content-type').indexOf('json') > -1) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    throw new Error('Response is not valid JSON file')
+                };
             } else {
-                reject(xhr.status);
-            }
-        })
-    })
-}
+                reject(JSON.parse(xhr.responseText));
+            };
+        });
+    });
+};
